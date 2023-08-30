f343b9f2bdbd1d649d7dca0edd98ad0bcd4a348d
anagelberg <amy.nagelberg@gmail.com>
2023-08-30 13:50:52 -0700

    add user jar endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,23 @@
 const knex = require("knex")(require("../knexfile"));
 
+const validUser = (userId) => {
+  // expand logic later to accomodate more users.
+  return userId === 1;
+};
+
 const getJars = (req, res) => {
-  res.send("this will return a list of jars the user is contributing to");
+  const userId = Number(req.params.userid);
+  if (validUser(userId)) {
+    knex("jar_user_join")
+      .select("jar_id", "name", "creator_id")
+      .where({ user_id: userId })
+      .join("jar", "jar.id", "=", "jar_user_join.jar_id")
+      .then((data) => {
+        res.send(data);
+      });
+  } else {
+    res.status(404).send("Invalid user id");
+  }
 };
 
 const editUserMovieData = (req, res) => {
