66e5f2338df2bb1120225f0570bf3970c9014e84
anagelberg <amy.nagelberg@gmail.com>
2023-12-14 21:50:17 -0800

    try saving session before redirect

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,28 +19,43 @@ router.get('/status', (req, res) => {
 router.get('/google/callback', passport.authenticate('google'), (req, res) => {
     console.log("Made it to /google/callback");
 
-    // Manually set the session cookie
-    const sessionCookieName = 'connect.sid'; // Default name, adjust if you've changed it
-    const sessionCookieValue = req.sessionID;
-    const sessionCookieOptions = {
-        maxAge: 24 * 60 * 60 * 1000, // Example: 24 hours
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'None',
-        domain: process.env.NODE_ENV === 'production' ? '.moviejar.ca' : undefined
-    };
 
-    res.cookie(sessionCookieName, sessionCookieValue, sessionCookieOptions);
-    console.log("Session cookie manually set");
-    res.redirect(process.env.FRONTEND_URL);
+    console.log("req.sessionID: ", req.sessionID);
+    console.log("req.session: ", req.session);
+
+    // // Manually set the session cookie
+    // const sessionCookieName = 'connect.sid'; // Default name, adjust if you've changed it
+    // const sessionCookieValue = req.sessionID;
+    // const sessionCookieOptions = {
+    //     maxAge: 24 * 60 * 60 * 1000, // Example: 24 hours
+    //     httpOnly: true,
+    //     secure: process.env.NODE_ENV === 'production',
+    //     sameSite: 'None',
+    //     domain: process.env.NODE_ENV === 'production' ? '.moviejar.ca' : undefined
+    // };
+
+    // res.cookie(sessionCookieName, sessionCookieValue, sessionCookieOptions);
+
+    // console.log("Session cookie manually set");
+
+    req.session.save(err => {
+        if (err) {
+            console.log("Error saving session: ", err)
+            return;
+        }
+        res.redirect(process.env.FRONTEND_URL);
+    });
+
+
+    // res.redirect(process.env.FRONTEND_URL);
 });
 
 
 router.get('/logout', (req, res) => {
     req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
+        // if (err) {
+        //     return next(err);
+        // }
         res.clearCookie('connect.sid');
         res.redirect(process.env.FRONTEND_URL);
     });
